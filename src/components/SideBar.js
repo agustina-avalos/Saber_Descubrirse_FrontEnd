f354c4aab48ba0c4b36e5a1f8c3ef10367de7d55
agustina-avalos <aguavalos@hotmail.com>
2025-09-06 18:19:52 +0200

    add profile form

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -3,22 +3,28 @@ import PropTypes from 'prop-types'
 import styles from '../css/SideBar.module.css'
 import { FaUser, FaCalendarAlt, FaBell, FaEnvelope } from "react-icons/fa";
 
-function SideBar({ user, menuItems }) {
+function SideBar({ user, menuItems,onSelect, selected}) {
   return (
     <div className={styles.sidebar}>
       {/* Usuario */}
       <div className={styles.user}>
-        <div className={styles.avatar}></div>
+        <div className={styles.avatar}>
+            {user.profileImg ? <img src={user.profileImg} alt={user.name} /> : <span>foto perfil</span>} 
+        </div>
         <span className={styles.username}>{user.name}</span>
       </div>
 
       {/* Menú */}
       <nav className={styles.menu}>
-        {menuItems.map((item, idx) => (
-          <a key={idx} href={item.href} className={styles.menuItem}>
+        {menuItems.map((item) => (
+          <button
+            key={item.id}
+            onClick={() => onSelect(item.id)}
+            className={`${styles.menuItem} ${selected === item.id ? styles.active : ""}`}
+          >
             {item.icon}
             {item.label}
-          </a>
+          </button>
         ))}
       </nav>
     </div>
--- src/pages/AdminPage.js
+++ src/pages/AdminPage.js
@@ -1,24 +1,48 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import { FaUser, FaCalendarAlt, FaBell, FaEnvelope } from "react-icons/fa";
 import SideBar from '../components/SideBar';
 import Navbar from '../components/Navbar';
+import { useState } from 'react';
+import patriciaImg from './../css/fotoPatri.png'
+import style from "./../css/AdminPage.module.css"
+import ProfileForm from '../components/ProfileForm';
+
 
 function AdminPage() {
- const user = { name: "Patricia Larrosa" };
-  const menuItems = [
-    { icon: <FaUser />, label: "Mi Perfil", href: "/perfil" },
-    { icon: <FaCalendarAlt />, label: "Calendario", href: "/calendario" },
-    { icon: <FaBell />, label: "Novedades", href: "/novedades" },
-    { icon: <FaEnvelope />, label: "Mensajes", href: "/mensajes" },
-  ];
+    const [selected, setSelected] = useState("perfil"); 
+
+
+    const user = { name: "Patricia Larrosa", profileImg:patriciaImg};
+    const menuItems = [
+        { icon: <FaUser />, label: "Mi Perfil",id: "perfil"},
+        { icon: <FaCalendarAlt />, label: "Calendario" ,id: "calendario"},
+        { icon: <FaBell />, label: "Novedades",id: "novedades" },
+        { icon: <FaEnvelope />, label: "Mensajes", id: "mensajes" },
+    ];
+
+     const renderForm = () => {
+        switch (selected) {
+        case "perfil":
+            return <ProfileForm></ProfileForm>
+        case "calendario":
+            return <div>Calendario</div>
+        case "novedades":
+            return <div>Novedades Form</div>
+        case "mensajes":
+            return <div>Mensajes Form</div>
+        default:
+            return <div>Selecciona una opción</div>;
+        }
+    };
 
   return (
-    <div>
+    <div className={style.container}>
         <Navbar></Navbar>
+
         <div style={{ display: "flex" }}>
-        <SideBar user={user} menuItems={menuItems} />
-        <main style={{ flex: 1, padding: "1rem" }}>Contenido principal</main>
+            <SideBar user={user} menuItems={menuItems} onSelect={setSelected} selected={selected} />
+            <main style={{ flex: 1, padding: "1rem" }}>
+                {renderForm()}</main>
         </div>
     </div>
   );
