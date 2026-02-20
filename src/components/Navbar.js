f219f40691d3863a26a4a90f206ebd428908c1f6
agustina-avalos <aguavalos@hotmail.com>
2026-02-20 23:33:02 +0100

    new brand

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,7 +9,7 @@ function Navbar() {
     <nav className={styles.navbar}>
       <ul className={styles.navList}>
         <Link to="/" className={styles.navItem}><a className={styles.link} href="#services-carousel">Nuestros servicios</a></Link>
-        <Link  to="/professional"  className={styles.navItem}><a className={styles.link} href="#professionals">Profesionales</a></Link>
+        <Link  to="/professional"  className={styles.navItem}><a className={styles.link} href="#professionals">Sobre mi</a></Link>
         <Link   to="/contact"  className={styles.navItem}><a className={styles.link} href="#contact">Contacto</a></Link>
         <Link className={styles.navItem}><a className={styles.link} href="#research">Suscripciones</a></Link>
         <Link to="/admin" className={styles.navItem}><button className={styles.button}>Ingresar</button></Link>
